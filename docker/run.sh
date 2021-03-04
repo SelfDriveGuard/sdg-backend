b1e62cf5cbea8ecc73476c7fc3a722fd0bee5bee
yuenshuning <yuenshuning@163.com>
2021-03-04 15:47:19 +0800

    confict

--- docker/run.sh
+++ docker/run.sh
@@ -8,12 +8,12 @@ trap cleanup SIGINT
 trap cleanup SIGTERM
 trap cleanup KILL
 
+mongod --dbpath=/var/lib/mongodb --port 8094 --bind_ip 0.0.0.0 --auth --logpath=/var/log/mongodb/mongodb.log --fork
+
 cd /home/carla/ADTest_backend
 
 echo "Launching ADTest_backend."
-mongod --dbpath=/var/lib/mongodb --port 8094 --bind_ip 0.0.0.0 --auth --logpath=/var/log/mongodb/mongodb.log --fork
 node app.js &
 sleep 5
-
 echo "ADTest_backend launched."
 sleep infinity
