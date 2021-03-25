3dfaaa6dee389a6ac29a068dfcfb4086f86aaa31
崔原广 <>
2021-03-25 15:31:35 +0800

    唯一url

--- config/mongo.js
+++ config/mongo.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const option = {
     autoReconnect: true,
 };
-// const url = "mongodb://cygRoot:cyg1tracy@118.31.126.252:29089/ADTest?authSource=admin";
-// const url = "mongodb://localhost:27017/SelfDriveGuard";
+
 const url = "mongodb://sdg:123456@localhost:8094/SelfDriveGuard?authSource=admin";
 
 module.exports = {
