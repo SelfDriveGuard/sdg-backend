310920b30ad54f5fbf63a8461e3b370ebf66daf9
崔原广 <>
2021-05-13 10:58:11 +0800

    打分机制

--- routes/example.js
+++ routes/example.js
@@ -2,7 +2,6 @@ const router = require('koa-router')();
 const fs = require('fs');
 const path = require('path');
 
-
 // 查看场景库代码
 router.get('/', async (ctx) => {
     const {name} = ctx.request.query;
